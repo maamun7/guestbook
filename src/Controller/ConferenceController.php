14a8e9c8780c05b80e764bb7d6d51f00cbb3525d
Mamun Ahmed <maamun7@gmail.com>
2020-11-10 21:45:36 +0600

    Done entity building and data render !

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ConferenceController extends AbstractController
+{
+	/**
+	 * @Route("/conference", name="conference")
+	 */
+	public function index(): Response
+	{
+		return $this->render('conference/index.html.twig', [
+			'controller_name' => 'ConferenceController',
+		]);
+	}
+	
+	/**
+	 * @Route("/conference/{id}", name="conference")
+	 */
+	public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+	{
+		$offset = max(0, $request->query->getInt('offset', 0));
+		$paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+		return $this->render('conference/show.html.twig', [
+			'conference' => $conference,
+			'comments' 	=> $paginator,
+			'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+			'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+		]);
+	}
+}
---
+++ src/Repository/CommentRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Comment|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Comment[]    findAll()
+ * @method Comment[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CommentRepository extends ServiceEntityRepository
+{
+	public const PAGINATOR_PER_PAGE = 1;
+
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Comment::class);
+	}
+	
+	public function getCommentPaginator(Conference $conference, int $offset): Paginator
+	{
+		$query = $this->createQueryBuilder('c')
+			->andWhere('c.conference = :conference')
+			->setParameter('conference', $conference)
+			->orderBy('c.createdAt', 'DESC')
+			->setMaxResults(self::PAGINATOR_PER_PAGE)
+			->setFirstResult($offset)
+			->getQuery();
+
+		return new Paginator($query);
+	}
+}
